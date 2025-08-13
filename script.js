045b462c4e099b39ef9c6dc2eb6059f3f5bc887d
Pranita <pranitameghdambar9106@gmail.com>
2025-08-13 22:47:01 +0530

    collision detection 1.0 succssful

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ document.addEventListener("keydown", function (event) {
   }
 })
 
+const playerEle = document.querySelector(".dino")
+const obstacleEle = document.querySelector(".obstacle")
+
+let collided = false;
+function isCollision() {
+  const playerRect = playerEle.getBoundingClientRect();
+  const obstacleRect = obstacleEle.getBoundingClientRect();
+  // console.log(playerRect)
+  // console.log(obstacleRect)
+
+  const playerL = playerRect.left;
+  const playerR = playerRect.right;
+  const playerB = playerRect.bottom;
+
+  const obstacleL = obstacleRect.left;
+  const obstacleR = obstacleRect.right;
+  const obstacleT = obstacleRect.top;
+
+  const xCollision = obstacleL < playerR && playerL < obstacleR;
+  const yCollision = obstacleT < playerB; //to be updated later
+  collided = false
+  if (xCollision && yCollision)
+    collided = true
+  return collided;
+}
+
+function monitorCollision() {
+  setInterval(() => {
+    // console.log("checking")
+    if (!collided && isCollision()) {
+      console.log("collided")
+      collided = true
+      alert("You Died")
+    }
+  }, 10)
+}
+
+function main() {
+  monitorCollision();
+}
+main();
